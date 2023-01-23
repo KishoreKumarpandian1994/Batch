530452cfad9a20f6dbcee3265e44266942e499b4
kishorekumarpandian1994 <kishorekumar.mech27@gmailcom>
2023-01-23 13:25:30 +0530

    hello

---
+++ src/test/java/org/base/Sample.java
@@ -0,0 +1,15 @@
+package org.base;
+
+public class Sample {
+	private void method1() {
+		System.out.println("kishore");
+
+	}
+	private void method2() {
+		System.out.println("kishore");
+	
+	}
+	private void method3() {
+		System.out.println("kishore");
+	}
+}
